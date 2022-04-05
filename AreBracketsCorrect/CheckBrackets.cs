2bb63333f2f8786a4d50e7d291a3754e44b95676
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2022-04-05 21:20:35 +0300

    Добавьте файлы проекта.

---
+++ AreBracketsCorrect/CheckBrackets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AreBracketsCorrect
+{
+    class CheckBrackets
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AreBracketsCorrect("([)]"));
+        }
+        static bool AreBracketsCorrect(string inputString)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == '{' || inputString[i] == '(' || inputString[i] == '[')
+                    stack.Push(inputString[i]);
+
+                char tempsymb = stack.Peek();
+                if ((inputString[i] == '}' && tempsymb == '{') || (inputString[i] == ')' && tempsymb == '(') || (inputString[i] == ']' && tempsymb == '['))
+                    stack.Pop();
+            }
+            if (stack.Count == 0)
+                return true;
+            return false;
+        }
+    }
+}
